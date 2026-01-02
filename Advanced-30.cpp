bff5788beba64a4db5107688419496ec9c187135
Baraa <156128855+Baraa-op@users.noreply.github.com>
2026-01-02 17:18:58 +0300

    Update Advanced-30.cpp

--- Advanced-30.cpp
+++ Advanced-30.cpp
@@ -1,1 +1,37 @@
+//Advanced-30
+#include <iostream>
+#include <string>
+using namespace std  ;
 
+
+int Read_Positive_Numbers(string Message)
+{
+    int Number;
+    do
+    {
+        cout << Message  ;
+        cin >> Number ;
+    } while(Number < 0) ;
+    
+    return Number;
+}
+
+int Factorial(int N)
+{
+
+    int F = 1 ;
+    for(int Counter = N ; Counter >= 1 ; Counter--)
+    {
+        F = F * Counter;
+    }
+    
+    return F ;
+}
+
+int main() {
+    
+    
+    
+   cout <<  Factorial(Read_Positive_Numbers("plese enter positive number : ") ) ;
+    
+}
