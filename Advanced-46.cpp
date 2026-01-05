be3fcb4f80950abb853f5fabcfdfba605f1d8ad4
Baraa <156128855+Baraa-op@users.noreply.github.com>
2026-01-05 14:45:49 +0300

    Add function to print letters A to Z

---
+++ Advanced-46.cpp
@@ -0,0 +1,19 @@
+//Advanced-46
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std  ;
+
+void Print_Letters_AtoZ()
+{
+    for(int i = 65 ; i <= 90 ; i++)
+    {
+        cout << char(i) << "\n" ;
+    }
+}
+
+
+int main()
+{
+   Print_Letters_AtoZ();
+}
